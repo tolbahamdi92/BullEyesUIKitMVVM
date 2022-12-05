2ed7cd0cdd8c6943556d563f1f880197f68d548f
Tolba Hamdi <abokhadiga22@gmail.com>
2022-12-05 15:37:57 +0200

    Initial Commit

---
+++ BullEyesUIKitMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BullEyesUIKitMVVM
+//
+//  Created by Tolba on 11/05/1444 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
